a8bc548489313ba214dd7b3f1db77be7ba5ce4e1
finteger <nashassociatesinc@gmail.com>
2025-10-06 11:16:38 -0600

    Update

---
+++ tny_script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Todd Nash 
+   Date: October 2, 2025
+
+*/
