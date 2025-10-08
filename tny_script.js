3334f72bd38552a8b3df8d897fee40120e57cefe
finteger <nashassociatesinc@gmail.com>
2025-10-08 10:41:44 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -10,3 +10,12 @@
    Date: October 2, 2025
 
 */
+
+//Display current date & time
+document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+
+//Display the time left until New Years Eve Bash
+document.getElementById('days').textContent ="dd";
+document.getElementById('hrs').textContent ="hh";
+document.getElementById('mins').textContent ="mm";
+document.getElementById('secs').textContent ="ss";
